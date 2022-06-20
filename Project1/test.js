fb45265af4feb4cdc102d14547699e5bd5a426dc
sohashi <sohashi@ucsc.edu>
2022-06-20 01:41:21 -0700

    co co co commit

---
+++ Project1/test.js
@@ -0,0 +1,12 @@
+// const url = "https://opendata.resas-portal.go.jp/api/v1/prefectures";
+// const response = fetch(url, {"X-API-KEY" : "F6uHbU8bg4XP9sJBiZVGg3KL3vroy4Lyj8XLOHUE"});
+// const json = response.json();
+
+
+const promise1 = new Promise((resulve, reject) => {
+    throw 'Uh-oh!';
+});
+
+promise1.catch((error) => {
+    console.error(error);
+});
